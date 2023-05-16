bb2567f6d4c3e7cb7dd068caa59e41ac23e5d03f
Danial Alhammoud <danial0938162@gmail.com>
2023-05-16 07:35:54 +0300

    change baseUrl

--- next.config.js
+++ next.config.js
@@ -25,8 +25,8 @@ module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
     return config;
   },
   env: {
-    // SITE_URL: "http://teststudent.hiast.edu.sy:3000",
-    SITE_URL: "http://localhost:3000",
+    SITE_URL: "http://teststudent.hiast.edu.sy:3000",
+    // SITE_URL: "http://localhost:3000",
     SECRET: "LlKq6ZtYbr+hTC021nAnOj2/h2HwMfsFo4hrfCx6gts=",
     // BASE_URL: "http://172.25.1.71/student_affairs_backend/student_affairs/public",
     // BASE_URL: "http://student.hiast.edu.sy/backend/public",
